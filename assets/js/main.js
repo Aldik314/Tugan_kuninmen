8bdfded314525bcfb8b79e1b10c24db54aaee321
Aldik314 <aidanak100@gmail.com>
2026-04-16 23:19:18 +0300

    set page in js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,10 @@ let pageWidth = 800;
 let pageHeight = 550;
 
 let context;
+
+window.onload = function() {
+    page = document.getElementById("page");
+    page.height = pageHeight;
+    page.width = pageWidth;
+    context = page.getContext("2d"); //used for drawing
+}
