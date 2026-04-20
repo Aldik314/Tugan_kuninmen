7b738ae7d0e4431489d2de616493b7cc3e60ecc0
Aldik314 <aidanak100@gmail.com>
2026-04-20 16:09:45 +0300

    some changes in arrangement

--- assets/js/resource.js
+++ assets/js/resource.js
@@ -2,9 +2,9 @@ class Resources {
     constructor() {
         // To download everything we need
         this.toLoad = {
-            game_border: "./assets/img/game_b.png",
-            background_clouds: "./assets/img/bg_m.png",
-            background_ground: "./assets/img/bg.png"
+            game_border: "./assets/img/bg/game_b.png",
+            background_clouds: "./assets/img/bg/bg_m.png",
+            background_ground: "./assets/img/bg/bg.png"
         };
 
         // To keep the images
