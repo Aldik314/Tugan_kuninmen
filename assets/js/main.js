ab04f52276521a84326b0ead9b59977899d0223f
Aldik314 <aidanak100@gmail.com>
2026-05-14 20:30:14 +0300

    adding a character

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,30 @@ canvas.width = 814;
 canvas.height = 600;
 
 let cloudOffset = 0;
-const CLOUD_SPEED = 0.03;
+const CLOUD_SPEED = 0.01;
+
+const frames = [];
+const frameCount = 15;
+
+for (let i = 1; i <= frameCount; i++) {
+    frames.push(`./assets/img/m_sitting/${i}.png`);
+}
+
+let frameIndex = 0;
+const baseImage = new Image();
+baseImage.src = frames[0];
+
+setInterval(() => {
+    frameIndex = (frameIndex + 1) % frames.length;
+    baseImage.src = frames[frameIndex];
+}, 240);
+
+let isBaseImageLoaded = false;
+
+baseImage.onload = function() {
+    isBaseImageLoaded = true;
+    console.log("Base image loaded");
+};
 
 const draw = () => {
     const game_border = resources.images.game_border;
@@ -36,6 +59,10 @@ const draw = () => {
     const background_ground = resources.images.background_ground;
     if (background_ground.isLoaded) {
         ctx.drawImage(background_ground.image, 6, 42)
+    }
+
+    if (isBaseImageLoaded) {
+        ctx.drawImage(baseImage, 6, 42)
     }
 }
 
